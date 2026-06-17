8de3bc308c27842154740969d7b35595abb9c795
benalian42 <newtonfnied@gmail.com>
2026-06-17 07:56:11 -0300

    enviando o footer, card de produto e a home, talvez ao fazer merge seja necessário resolver conflitos, mas vamos ver

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import './App.css'
 import { Routes, Route } from 'react-router-dom'
 import { Header } from './components/Header.jsx'
+import { Footer } from './components/Footer.jsx'
 import { Home } from './pages/Home.jsx'
 import { Cart } from './pages/Cart.jsx'
 
@@ -14,6 +15,7 @@ function App() {
         <Route path="/home" element={<Home />} />
         <Route path="/cart" element={<Cart />} />        
       </Routes>
+      <Footer />
     </>
   )
 }
